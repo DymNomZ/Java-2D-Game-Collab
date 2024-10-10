a1b8c55d303cee41da37781e74795e343f7f72a2
BreakfasteMeat <133184897+BreakfasteMeat@users.noreply.github.com>
2024-10-10 18:29:28 +0800

    MAP_ENTITY constructors

--- classes/entities/MAP_ENTITY.java
+++ classes/entities/MAP_ENTITY.java
@@ -7,6 +7,15 @@ public abstract class MAP_ENTITY {
     private float haste;
     private int defense_stat;
     private int id;
+    public MAP_ENTITY(){}
+    public MAP_ENTITY(String name, int hit_points, int attack_stat, float haste, int defense_stat, int id){
+        this.name = name;
+        this.hit_points = hit_points;
+        this.attack_stat = attack_stat;
+        this.haste = haste;
+        this.defense_stat = defense_stat;
+        this.id = id;
+    }
 
     public String getName() {
         return name;
