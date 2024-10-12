49b7941f334507622592a61176e6c4e20265e252
BladeLucas <115134677+BladeLucas27@users.noreply.github.com>
2024-10-12 21:18:31 +0800

    Update ITEM.java
    Added a constructor for weapon&statmods, and a constructor for consumables, and also comments for int quantity and int quality
    -Raymond

--- classes/items/ITEM.java
+++ classes/items/ITEM.java
@@ -3,10 +3,30 @@ package classes.items;
 public abstract class ITEM {
     private String name;
     private boolean isStackable;
-    private int quantity;
+    private int quantity;   //suggest to move to only CONSUMABLE since no reason to give weapons and stat mods quantity since limited to only 1? -Raymond
     private String item_type;
-    private int quality;
+    private int quality;    //suggest to move to only WEAPONS since no reason to give consumables quality level? -Raymond
     private int id;
+
+    public ITEM(String name, String item_type, int quality, int id)    //for weapons and stat-modifiers -Raymond
+    {
+        this.name = name;
+        this.isStackable = false;
+        this.quantity = 1;
+        this.item_type = item_type;
+        this.quality = quality;
+        this.id = id;
+    }
+    public ITEM(String name, int quantity, String item_type, int id)    //for consumables   -Raymond
+    {
+        this.name = name;
+        this.isStackable = true;
+        this.quantity = quantity;
+        this.item_type = item_type;
+        this.quality = 1;
+        this.id = id;
+    }
+
 
     public String getName() {
         return name;
@@ -19,4 +39,6 @@ public abstract class ITEM {
     public int getId() {
         return id;
     }
+
+
 }
